6876fdf3e617865e7c1745fb7fcdb7b31399d09a
Gabriel Rodrigues <rodriguesgasoares@gmail.com>
2022-02-11 18:48:55 -0300

    corrigindo um problema

--- script.js
+++ script.js
@@ -6,20 +6,28 @@ function mostrarRGB() {
     res.innerHTML = ` rgb(${r}, ${g}, ${b})`
     document.body.style.background = `rgb(${r}, ${g}, ${b})`
     document.querySelector('#rgb').style.display = 'flex'
+    document.querySelector('#hex').style.display = 'none'
+    document.querySelector('#hsl').style.display = 'none'
 }
 
 function mostrarHEX() {
-    let res = document.querySelector('.res')
+    let res = document.querySelector('.reshex')
     let hex = Number.parseInt(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0') 
     res.innerHTML = `#${hex}`.toUpperCase()
     document.body.style.background = `#${hex}`
+    document.querySelector('#rgb').style.display = 'none'
+    document.querySelector('#hex').style.display = 'flex'
+    document.querySelector('#hsl').style.display = 'none'
 }
 
 function mostrarHSL() {
     let hue = parseInt(Math.random() * 101) 
     let sat = parseInt(Math.random() * 101)
     let lig = parseInt(Math.random() * 101)
-    let res = document.querySelector('.res')
+    let res = document.querySelector('.reshsl')
     res.innerHTML = `hsl(${hue}, ${sat}%, ${lig}%)`
     document.body.style.background = `hsl(${hue}, ${sat}%, ${lig}%)` 
+    document.querySelector('#rgb').style.display = 'none'
+    document.querySelector('#hsl').style.display = 'flex'
+    document.querySelector('#hex').style.display = 'none'
 }
